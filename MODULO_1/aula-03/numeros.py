5eaf41345e999e9b12e38d903fb2c271e947f68a
Gustavo Sorati <sorati.gustavo@gmail.com>
2024-07-16 12:48:59 -0300

    feat: aula 04 e 05 - tipo de numeros

---
+++ MODULO_1/aula-03/numeros.py
@@ -0,0 +1,44 @@
+# Tipo inteiro
+numero_inteiro = 3
+print("Inteiro =", numero_inteiro)
+
+# Real com ponto flutuante
+numero_real = 3.14
+print("Real com ponto flutuante =", numero_real)
+
+# type() -> Retorna o tipo da variável que estamos trabalhando
+print("Tipo da variável inteiro =", type(numero_inteiro))
+print("Tipo da variável float =", type(numero_real))
+
+# soma + 
+soma = 1 + 1
+print("1 + 1 =", soma)
+
+# subtração - 
+subtracao = 1 - 1
+print("1 - 1 =", subtracao)
+
+# multiplicação *
+multiplicacao = 2 * 2
+print("2 * 2 =", multiplicacao)
+
+# divisão /
+divisao = 10 / 2
+print("10 / 2 =", divisao)
+print("Tipo da variável do resultado da divisão =", type(divisao))
+
+# divisão inteiro /
+divisao = 5 // 2
+print("5 // 2 =", divisao)
+print("Tipo da variável do resultado da divisão =", type(divisao))
+
+# modulo %
+modulo = 10 % 2
+print("10 % 2 =", modulo)
+
+# conversão de valores
+conversao = int(divisao)
+print("Valor em inteiro (conversão) =", conversao)
+print("Valor em float (conversão) =", float(soma))
+
+
---
+++ MODULO_1/aula-03/variaveis.py
@@ -0,0 +1,11 @@
+""" Regras para declarar variáveis
+    - Letra minuscula e sem caracteres especiais.
+    - A tipagem é dinâmica, ou seja, não precisamos declarar o tipo da
+    da variável.
+
+    - O site PEP8 fornece um guia de boas práticas para o python.
+"""
+
+nome_completo = "Gustavo Sorati" # variáveis e funções
+nomeCompleto = "Gustavo Sorati" # classes
+idade = 29
