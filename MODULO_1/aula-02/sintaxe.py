10ca06259ddd418a2cc245ef0568c22f5ea794ff
Gustavo Sorati <sorati.gustavo@gmail.com>
2024-07-16 09:07:02 -0300

    feat: aula 02 sintaxe e blocos

---
+++ MODULO_1/aula-02/sintaxe.py
@@ -0,0 +1,10 @@
+# Comentário de uma única linha
+
+""" Esse é meu primeiro comentário
+    Essa é a segunda linha de comentário
+"""
+
+nome = "Gustavo" + \
+" Sorati"
+
+nome = "Gustavo" + " Sorati"
