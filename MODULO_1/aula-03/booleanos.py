50ccf652285951ff3e6d5946e3adda6861bd484e
Gustavo Sorati <sorati.gustavo@gmail.com>
2024-07-19 14:09:11 -0300

    feat: aula 06, tipo booleano

---
+++ MODULO_1/aula-03/booleanos.py
@@ -0,0 +1,12 @@
+# Comportam true ou false
+
+if True: 
+    print("Bloco IF vai ser executado.")
+else: 
+    print("Bloco ELSE não será executado.")
+
+if True and True:
+    print("Bloco será executado")
+
+if True or False:
+    print("Bloco será executado")
