290b8fcc5a657b8ffceb15ee3138f7a4f82e86d9
Gustavo Sorati <sorati.gustavo@gmail.com>
2024-07-19 14:12:00 -0300

    feat: complemento aula de textos

--- MODULO_1/aula-03/texto.py
+++ MODULO_1/aula-03/texto.py
@@ -18,3 +18,27 @@ print("Nome completo (7a forma): %s" % nome)
 print("Nome completo (8a forma): %s %s" % (nome, sobrenome)) # faz transformação e + segura
 print(f"Nome completo (9a forma): {nome} {sobrenome}") # + visualmente agradável
 print("Nome completo (10a forma): {} {}".format(nome, sobrenome))
+
+# Métodos
+
+# Informa a quantidade de ocorrências
+print(nome_completo_aspas.count("G"))
+
+# Retorna o índice da primeira aparição da palavra
+print(nome_completo_aspas.find("a"))
+
+# Converter a string para bytes
+
+print(nome.encode())
+print(nome.encode().decode())
+
+# Substituição
+
+print(nome.replace("G", "123"))
+
+print(nome.strip("G"))
+
+print(nome.startswith("Gu"))
+
+print("us" in nome)
+print("us" not in nome)
